0a4db62082c0710083d3a9dbfe849290f298a0e4
MDH <hurak.michal@gmail.com>
2026-06-07 14:47:14 +0200

    Add: Addon list for repository

---
+++ addons.py
@@ -0,0 +1,11 @@
+
+# list of addons that we are releasing
+
+__ADDONS__ = [
+    "repository.mdb",
+    "plugin.video.eagle.blvck",
+    "plugin.video.freeview.sk",
+    "plugin.video.prehrajto",
+    "plugin.video.sktonline",
+    "plugin.video.stream-cinema"
+]
